8352c8e4a06f2b2692c6c5dad71ee19b424ccdce
unknown <sbn9987@naver.com>
2021-04-13 19:09:21 +0900

    qyaho

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict"
 
-console.log("hi")
-console.log("hello")
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button")
+    // signupBtn = document.querySelector("signup")
+
+
+loginBtn.addEventListener("click",loginbutton)
+// signupBtn.addEventListener("click",signupbutton)
+
+function loginbutton(){
+       const req = {
+         id:id.value,
+         password: password.value
+       }
+       console.log(req)
+};
+
+// function signupbutton(){
+//     console.log("hello");
+// }
